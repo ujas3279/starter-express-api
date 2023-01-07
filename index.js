e4d86cedee281a38f0414aee7910ce4e60b41c62
Ujas Patel <41516784+ujas3279@users.noreply.github.com>
2023-01-07 20:16:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 var cron = require('node-cron');
 const axios = require('axios');
 
-cron.schedule('* 2 * * *', async () => {
+cron.schedule('1 * * * *', async () => {
     await axios.get(process.env.Business)
     .then((res) =>{ console.log(res.data.message)});
     await axios.get(process.env.Sports)
